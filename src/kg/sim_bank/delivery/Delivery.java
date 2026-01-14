c0d47a7f3e3ffe3a20d59c06edacc2267faa291d
kbnrma <kbnrma@gmail.com>
2026-01-14 10:24:48 +0600

    Resolved conflict

---
+++ src/kg/sim_bank/delivery/Delivery.java
@@ -0,0 +1,9 @@
+package kg.sim_bank.delivery;
+
+public class Delivery {
+    public static void main(String[] args) {
+
+        System.out.println("Delivery Yandex");
+        System.out.println("Delivery Glovo");
+    }
+}
