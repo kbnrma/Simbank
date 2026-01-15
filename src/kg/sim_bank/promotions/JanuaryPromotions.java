280caa0e17a799f5ac582cf9ec46e1ae6507284a
kbnrma <kbnrma@gmail.com>
2026-01-15 09:36:32 +0600

    General January Promo123.
    January Promotion1
    
    January Promotion2
    
    January Promotio3
    
    January Promotio4
    
    January Promotio5
    
    January Promotio6

---
+++ src/kg/sim_bank/promotions/JanuaryPromotions.java
@@ -0,0 +1,15 @@
+package kg.sim_bank.promotions;
+
+public class JanuaryPromotions {
+    public static void main(String[] args) {
+
+        System.out.println("January Promotion");
+        System.out.println("January Promotion123");
+        System.out.println("January Promotion1234");
+        System.out.println("January Promotion1235");
+        System.out.println("January Promotion12356");
+        System.out.println("January Promotion54321");
+        System.out.println("January Promotion321");
+        System.out.println("January Promotion321123");
+    }
+}
